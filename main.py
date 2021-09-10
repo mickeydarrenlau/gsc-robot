ca2566ecc05448c7e31a5bf175ac1da515cf36e4
mickeydarrenlau <darrenwjlau@gmail.com>
2021-09-10 09:22:42 +0800

    Darren Lau

---
+++ main.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def welcome():
+    return "200 ok"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)  # This line is required to run Flask on repl.it
