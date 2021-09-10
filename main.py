517e9ffb4b72362c0b9ffd3277c2d8986b931ee6
mickeydarrenlau <darrenwjlau@gmail.com>
2021-09-10 09:38:28 +0800

    Darren Lau

--- main.py
+++ main.py
@@ -7,5 +7,13 @@ def welcome():
     return "200-ok"
 
 
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    return {
+        "fulfillmentText": 'This is from the replit webhook',
+        "source": 'webhook'
+    }
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)  # This line is required to run Flask on repl.it
